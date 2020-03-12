da143b75a6ec8b09b282a50042f3296cedfdeedf
Strappazzon <strappazzon@protonmail.com>
2020-03-12 21:27:29 +0100

    Inline background randomizer script

--- assets/js/bg.js
+++
@@ -1,3 +0,0 @@
-let images = [ 'bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg', 'bg5.jpg' ];
-
-document.querySelector("html").style.backgroundImage = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(assets/img/" + images[Math.floor(Math.random() * images.length)] + ")";
